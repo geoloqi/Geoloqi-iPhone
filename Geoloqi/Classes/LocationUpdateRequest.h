dbe8b31bafe0207406c53976f701e0959af235d0
Aaron Parecki <aaron@parecki.com>
2010-08-04 22:24:03 -0700

    Tracks hardware version in 'client' JSON sent to server

--- Geoloqi/Classes/LocationUpdateRequest.h
+++ Geoloqi/Classes/LocationUpdateRequest.h
@@ -16,5 +16,6 @@
 }
 
 - (void)setLocationDataFromLocations:(NSArray *)locations;
+- (NSString *)hardware;
 
 @end
--- Geoloqi/Classes/LocationUpdateRequest.m
+++ Geoloqi/Classes/LocationUpdateRequest.m
@@ -10,6 +10,8 @@
 
 #import "CJSONDataSerializer.h"
 #import "ISO8601DateFormatter.h"
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 @implementation LocationUpdateRequest
 
@@ -55,6 +57,7 @@
 							   [bundleInfo objectForKey:@"CFBundleDisplayName"], @"name",
 							   [bundleInfo objectForKey:@"CFBundleVersion"], @"version",
 							   [NSString stringWithFormat:@"%@ %@", d.systemName, d.systemVersion], @"platform",
+							   [self hardware], @"hardware",
 							   nil]
 					   forKey:@"client"];
 		
@@ -65,5 +68,29 @@
 	
 	[self appendPostData:[[CJSONDataSerializer serializer] serializeArray:locationArray]];
 }
+
+- (NSString *)hardware
+{
+	size_t size;
+	
+	// Set 'oldp' parameter to NULL to get the size of the data
+	// returned so we can allocate appropriate amount of space
+	sysctlbyname("hw.machine", NULL, &size, NULL, 0); 
+	
+	// Allocate the space to store name
+	char *name = malloc(size);
+	
+	// Get the platform name
+	sysctlbyname("hw.machine", name, &size, NULL, 0);
+	
+	// Place name into a string
+	NSString *machine = [NSString stringWithCString:name encoding:NSUTF8StringEncoding];
+	
+	// Done with this
+	free(name);
+	
+	return machine;
+}
+
 
 @end
